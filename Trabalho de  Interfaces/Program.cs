62bdfb5e0a976940c6070d49b11b79bcaead8770
Yasmin <Yasmin@LAPTOP-RQ3HPU0S>
2023-12-03 00:58:59 -0300

    Adicionar arquivos de projeto.

---
+++ Trabalho de  Interfaces/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+using Trabalho_de_Interfaces;
+
+namespace Trabalho_de_Interfaces
+{
+
+
+    class Program
+    {
+        static void Main()
+        {
+            CalculadoraHyper hyper = new CalculadoraHyper();
+            Console.WriteLine(hyper);
+            double resultAdicao = hyper.Adicao(7.0, 3.0);
+            double resultSubtracao = hyper.Subtracao(5.0, 2.0);
+            double resultMultiplicacao = hyper.Multiplicacao(4.0, 9.5);
+            double resultadDivisao = hyper.Divisao(10.0, 7);
+            Console.WriteLine(resultAdicao);
+            Console.WriteLine(resultSubtracao);
+            Console.WriteLine(resultMultiplicacao);
+            Console.WriteLine(resultAdicao);
+        }
+    }
+}        
---
+++ Trabalho de  Interfaces/calculadoraHyper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_de_Interfaces
+{
+    public class CalculadoraHyper : ICalculadora<double>
+    {
+
+        public double Adicao(double numA, double numB)
+        {
+            return numA + numB;
+        }
+        public double Subtracao(double numA, double numB)
+        {
+            return numA - numB;
+        }
+        public double Multiplicacao(double numA, double numB)
+        {
+            return numA * numB;
+        }
+        public double Divisao(double numA, double numB)
+        {
+            if (numB == 0)
+            {
+                throw new ArgumentException("não tem como dividir");
+
+
+            }
+            return numA / numB;
+        }
+    }
+
+}
